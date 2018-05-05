5f7cfcc29b931b7160d2df9b0baaf2655f593fa3
mila99723 <36603790+mila99723@users.noreply.github.com>
2018-05-06 02:19:05 +0300

    Update functions.scala

--- laba6var19-3-4scala/functions.scala
+++ laba6var19-3-4scala/functions.scala
@@ -2,15 +2,18 @@ package laba6
 
 class functions {
 
+  //divide colletion in two: >0 and <0
   def partAboveZero (list:List[Int]): (List[Int], List[Int]) = {
     list.partition(_ > 0) // part 1 var 19 { (az, bz) az.foreach(println) bz.foreach(println) }
   }
 
+  //turning prelast element of the collection
   def preLastElem (list:List[Int]): Int = {
     // val prelast = list.reduceLeft(_ + _) - (list.reduceLeft(_ + _) - list.takeRight(2).reduceLeft(_ + _)) - list.takeRight(1).reduceLeft(_ + _)
     list.init.fold(0)(_ + _) - list.init.init.fold(0)(_ + _)
   }
 
+  //make list from Map elements with even key
   def mapValWithEvenKey (map:Map[Int, String]): List[String] = {
     map.filterKeys(_ % 2 == 0).values.toList
   }
